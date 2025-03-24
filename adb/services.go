efc2e0c914633140892147bd22e3a8d1995cbe9b
Patrick Gaskin <patrick@pgaskin.net>
2025-03-24 02:04:19 -0400

    adb: Add TODOs for services

---
+++ adb/services.go
@@ -0,0 +1,30 @@
+package adb
+
+// https://cs.android.com/android/platform/superproject/main/+/main:packages/modules/adb/daemon/services.cpp;drc=a9b3987d2a42a40de0d67fcecb50c9716639ef03
+
+// TODO: jdwp
+// TODO: track-jdwp
+// TODO: track-app
+// TODO: sink
+// TODO: source
+// TODO: abb
+// TODO: abb_exec
+// TODO: framebuffer
+// TODO: remount
+// TODO: reboot
+// TODO: root
+// TODO: unroot
+// TODO: backup
+// TODO: restore
+// TODO: disable-verity
+// TODO: enable-verity
+// TODO: tcpip
+// TODO: usb
+// TODO: dev
+// TODO: dev-raw
+// TODO: shell
+// TODO: exec
+// TODO: sync
+// TODO: reverse
+// TODO: reconnect
+// TODO: spin
