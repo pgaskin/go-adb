46df571cdcec51513b169d18ac60eb1ea8c66ca8
Patrick Gaskin <patrick@pgaskin.net>
2025-03-25 14:17:28 -0400

    adb/adbhost: Implement devices and track-devices

--- adb/adbhost/services.go
+++ adb/adbhost/services.go
@@ -2,6 +2,9 @@ package adbhost
 
 import (
 	"context"
+	"iter"
+
+	"github.com/pgaskin/go-adb/adb/adbproto"
 )
 
 // https://cs.android.com/android/platform/superproject/main/+/main:packages/modules/adb/services.cpp;drc=01cbbf505e3348a70cd846b26fae603bdf44b3c5
@@ -19,22 +22,90 @@ func Kill(ctx context.Context, srv *Dialer) error {
 	return nil
 }
 
+// Devices gets the list of devices using "host:devices" or "host:devices-l".
+// Note that this uses the text format internally, which means that not all
+// fields will be set and attributes will be sanitized.
+func Devices(ctx context.Context, srv *Dialer, long bool) ([]*TransportInfo, error) {
+	var svc string
+	if long {
+		svc = "host:devices-l"
+	} else {
+		svc = "host:devices"
+	}
+	conn, err := srv.DialADBHost(ctx, svc)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	buf, err := adbproto.ReadProtocolBytes(conn, nil)
+	if err != nil {
+		return nil, adbproto.ProtocolErrorf("read device list: %w", err)
+	}
+	return ParseDevices(buf)
+}
+
+// TrackDevices tracks the devices connected to the server in real-time. This
+// will use the text format internally, which means that not all fields will be
+// set and attributes will be sanitized.
+//
+//	var err error
+//	for info := range adbhost.TrackDevices(ctx, srv, true)(&err) {
+//	    if stop {
+//	        break
+//	    }
+//	    fmt.Println(info)
+//	}
+//	if err != nil {
+//	    panic(err)
+//	}
+func TrackDevices(ctx context.Context, srv *Dialer, long bool) func(*error) iter.Seq[[]*TransportInfo] {
+	return newErrIter(func(yield func([]*TransportInfo) bool) error {
+		var svc string
+		if long {
+			svc = "host:track-devices-l"
+		} else {
+			svc = "host:track-devices"
+		}
+		if long && srv.SupportsFeature(adbproto.FeatureDeviceTrackerProtoFormat) {
+			// TODO
+		}
+
+		conn, err := srv.DialADBHost(ctx, svc)
+		if err != nil {
+			return err
+		}
+		defer conn.Close()
+
+		var buf []byte
+		for {
+			buf, err = adbproto.ReadProtocolBytes(conn, buf[:0])
+			if err != nil {
+				return adbproto.ProtocolErrorf("read next device tracker line: %w", err)
+			}
+			devs, err := ParseDevices(buf)
+			if err != nil {
+				return adbproto.ProtocolErrorf("parse device tracker line: %w", err)
+			}
+			if !yield(devs) {
+				return nil
+			}
+		}
+	})
+}
+
 // host request
 // TODO: server-status
-// TODO: devices, devices-l
 // TODO: reconnect-offline
-// TODO: host-features
 // TODO: disconnect
 // TODO: version
 // TODO: emulator
 
 // host socket
-// TODO: track-devices (short, long, proto-binary, proto-text)
 // TODO: connect
 // TODO: pair
 
 // host transport
-// TODO: features
 // TODO: disconnect
 // TODO: get-serialno
 // TODO: get-devpath
@@ -49,3 +120,13 @@ func Kill(ctx context.Context, srv *Dialer) error {
 // TODO: mdns:services
 
 // note: reverse works by updating a whitelist on the host, and the device opens tcp:... services in reverse
+
+func newErrIter[T any](seq func(yield func(T) bool) error) func(*error) iter.Seq[T] {
+	return func(err *error) iter.Seq[T] {
+		return func(yield func(T) bool) {
+			*err = seq(func(v T) bool {
+				return yield(v)
+			})
+		}
+	}
+}
