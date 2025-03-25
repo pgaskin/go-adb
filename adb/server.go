1b07a610d2e10e98a53f02114e1d31aeca32befb
Patrick Gaskin <patrick@pgaskin.net>
2025-03-25 11:50:15 -0400

    adb: Add features

--- adb/server.go
+++ adb/server.go
@@ -4,6 +4,8 @@ package adb
 import (
 	"context"
 	"net"
+
+	"github.com/pgaskin/go-adb/adb/adbproto"
 )
 
 // Dialer connects to a service on an ADB server (i.e., device).
@@ -12,4 +14,18 @@ import (
 // establishment. Once the conn is returned, the context no longer affects it.
 type Dialer interface {
 	DialADB(ctx context.Context, svc string) (net.Conn, error)
+}
+
+// Features is an optional interface which can be implemented by a [Dialer].
+type Features interface {
+	SupportsFeature(f adbproto.Feature) bool
+}
+
+// SupportsFeature returns true if the dialer implements [Features] and supports
+// the specified feature.
+func SupportsFeature(d Dialer, f adbproto.Feature) bool {
+	if df, ok := d.(Features); ok && df != nil {
+		return df.SupportsFeature(f)
+	}
+	return false
 }
