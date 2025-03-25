44978a6f39bbc9944d1f22d0ded6c6fee76ac36f
Patrick Gaskin <patrick@pgaskin.net>
2025-03-25 11:37:02 -0400

    adb/adbhost: Implement kill service

--- adb/adbhost/services.go
+++ adb/adbhost/services.go
@@ -1,11 +1,25 @@
 package adbhost
 
+import (
+	"context"
+)
+
 // https://cs.android.com/android/platform/superproject/main/+/main:packages/modules/adb/services.cpp;drc=01cbbf505e3348a70cd846b26fae603bdf44b3c5
 // https://cs.android.com/android/platform/superproject/main/+/main:packages/modules/adb/adb.cpp;l=1275-1616;drc=9f298fb1f3317371b49439efb20a598b3a881bf3
 // https://cs.android.com/android/platform/superproject/main/+/main:packages/modules/adb/adb.cpp;l=1133-1242;drc=9c843a66d11d85e1f69e944f1b37314d3e47aab1
 
+// Kill kills the ADB server. This may fail if ADB_REJECT_KILL_SERVER=1 is set
+// on the server.
+func Kill(ctx context.Context, srv *Dialer) error {
+	conn, err := srv.DialADBHost(ctx, "host:kill")
+	if err != nil {
+		return err
+	}
+	conn.Close()
+	return nil
+}
+
 // host request
-// TODO: kill
 // TODO: server-status
 // TODO: devices, devices-l
 // TODO: reconnect-offline
