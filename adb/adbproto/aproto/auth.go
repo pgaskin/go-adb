ccd476d54db31d41f1bdb75b69d49c6103af90bd
Patrick Gaskin <patrick@pgaskin.net>
2025-07-04 01:44:46 -0400

    adb/adbproto/aproto: Implement pubkey parsing

---
+++ adb/adbproto/aproto/auth.go
@@ -0,0 +1,36 @@
+package aproto
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+// ParsePublicKey parses an ADB public key.
+func ParsePublicKey(buf []byte) (key *PublicKey, name string, err error) {
+	// split on space or tab
+	for i, b := range buf {
+		if b == ' ' || b == '\t' {
+			name = string(buf[i+1:])
+			buf = buf[:i]
+			break
+		}
+	}
+
+	// check length
+	if act, exp := len(buf), base64.StdEncoding.EncodedLen(PublicKeyEncodedSize); act != exp {
+		return nil, name, fmt.Errorf("incorrect encoded pubkey length (act=%d exp=%d)", act, exp)
+	}
+
+	// decode pubkey base64
+	var tmp [PublicKeyEncodedSize]byte
+	if _, err := base64.StdEncoding.Decode(tmp[:], buf); err != nil {
+		return nil, name, err
+	}
+
+	// decode pubkey
+	key = new(PublicKey)
+	if err := key.UnmarshalBinary(tmp[:]); err != nil {
+		return nil, name, err
+	}
+	return key, name, err
+}
